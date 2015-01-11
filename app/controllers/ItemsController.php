b14194b2a8898c791dac8f01d1018df041c81da0
Tim Jones <tdjones74021@yahoo.com>
2015-01-11 17:47:27 -0600

    Updated implementation of 'download' method in ItemsController, including adding GetID3 support.

--- app/controllers/ItemsController.php
+++ app/controllers/ItemsController.php
@@ -1,5 +1,7 @@
 <?php
 
+use GetId3\GetId3Core as GetId3;
+
 class ItemsController extends \BaseController {
 
 	/**
@@ -104,6 +106,38 @@ class ItemsController extends \BaseController {
          public function download() {
              $input = Input::get('item');
              Kint::dump($input);
+             
+             $item = Item::find($input);
+             
+             /*
+              * Notes:
+              * 12/19/2010 and before (Session IDs 1 - 386):  On Dot 5 Hosting site at 128kbps.
+              * After 12/19/2010 - 04/17/2011 (Session IDs 387 - 430):  On Dot 5 Hosting site at 64kbps.
+              * After 04/17/2011 (Session IDs 431 and greater):  On Dreamhost site at 64kbps.
+              */
+             
+             if ( $item->id < 387 ) {
+                 $dl_url = 'http://www.workshopmultimedia.com/memorial_drive/' . str_ireplace('.mp3', '', $item->filename_base) . '_128kbps.mp3';
+             } elseif ( $item->id < 431 ) {
+                 $dl_url = 'http://www.workshopmultimedia.com/memorial_drive/' . str_ireplace('.mp3', '', $item->filename_base) . '_64kbps.mp3';
+             } else {
+                 $dl_url = 'http://www.workshopmultimedia.net/memorial_drive/' . str_ireplace('.mp3', '', $item->filename_base) . '_64kbps.mp3';
+             }
+             
+             // Update the download count
+             $item->num_downloads += 1;
+             $item->save();
+             
+             $id3 = new GetId3();
+             $audio = $id3->setOptionMD5Data(TRUE)
+                        ->setOptionMD5DataSource(TRUE)
+                        ->setEncoding('UTF-8')
+                        ->analyze($dl_url);
+             
+             Kint::dump($audio);
+             
+             //if ( !isset($audio['error']) )
+                return Response::download($dl_url, urlencode( $item->session_title . ' - ' . $item->speaker_name . ' - ' . $item->session_date . '.mp3' ));
          }
 
 
