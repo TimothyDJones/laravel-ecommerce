98e955eb27c9ed3a06975fac662b77bc7a0e4175
Tim Jones <tdjones74021@yahoo.com>
2015-01-22 15:21:52 -0600

    Add different attribution on screen for 'CD', 'DVD', and 'SET' product types.

--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -6,7 +6,16 @@
                 @foreach ( $products as $product )
                     @if ( (int) $product->available_ind > 0 )
                         <div class="item item-6">
-                            <h3>{{ $product->form_id }}  {{ $product->workshop_year }}</h3>
+                            <h3>
+                                @if ( $product->prod_type == 'DVD' )
+                                    <strong>{{ $product->form_id }}</strong>
+                                @elseif ( $product->prod_type == 'SET' )
+                                    <span style="color: burlywood;"><strong>{{ $product->form_id }}</strong></span>
+                                @else
+                                    {{ $product->form_id }}
+                                @endif
+                                &nbsp;&nbsp;{{ $product->workshop_year }}
+                            </h3>
                             <h2>{{ $product->session_title }}</h2>
                             <h3>{{ $product->speaker_first_name }} {{ $product->speaker_last_name }}</h3>
                             <h3>Price: ${{ (int) $product->price }} ea.</h3>
