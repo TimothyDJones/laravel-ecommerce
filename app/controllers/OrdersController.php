d57da46ff9421de530bcacceb7879f409b809a87
Tim Jones <tdjones74021@yahoo.com>
2015-01-26 17:08:45 -0600

    Add order checkout functionality to display cart and shipping options.  Add functions to OrdersController for calculating the number of items by type (CD, DVD, or MP3) and associated valid shipping options.

--- app/controllers/OrdersController.php
+++ app/controllers/OrdersController.php
@@ -1,7 +1,10 @@
 <?php
 
 class OrdersController extends \BaseController {
-
+        
+        private $order_id = 0;
+        private $customer_id = 0;
+        
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -20,7 +23,12 @@ class OrdersController extends \BaseController {
 	 */
 	public function create()
 	{
-		//
+            if ( Auth::check() ) {
+                $cartContents = Cart::contents();
+                $shipping_options = OrdersController::getShippingOptions();
+                $this->layout->content = View::make('orders.create', compact('cartContents', 'shipping_options'));
+            }
+            
 	}
 
 
@@ -89,8 +97,32 @@ class OrdersController extends \BaseController {
 	 * @return Response
 	 */        
         public function checkout() {
-            OrdersController::createShellOrder();
-            OrdersController::persistCart();
+            // If user is not logged in, then re-direct to create account and
+            // address, if necessary.
+            if ( Auth::guest() ) {
+                // Set a session variable to indicate that we are in the
+                // checkout process.
+                Session::put('checkOutInProgress', TRUE);
+                Redirect::route('customers.create');
+            }
+            
+            // If user is logged in, but doesn't have address and is ordering
+            // physical media (not MP3s ONLY!), then re-direct to add address.
+            if ( Auth::check() ) {
+                $query = Address::where('customer_id', '=', Auth::id());
+                $addr = $query->get()->first();
+                if ( !$addr->id ) {
+                    Redirect::route('customers.address.create', array('id' => Auth::id()));
+                }
+            }
+            
+            // If everything with customer and address is good, we re-direct
+            // to creating the shell order, including choosing shipping method
+            // and any notes.
+            return Redirect::route('orders.create');
+            
+            //OrdersController::createShellOrder();
+            //OrdersController::persistCart();
         }
         
         private function persistCart() {
@@ -101,5 +133,78 @@ class OrdersController extends \BaseController {
             $order = new Order();
             $order->customer_id = Input::get('customer_id');
             $order->order_status = 'Created';
+        }
+        
+        private function getShippingOptions() {
+            $shipping_options = array();
+            
+            $count = OrdersController::getCountOfItems();
+            
+            if ( $count['CD'] > 0 && $count['DVD'] > 0 ) {
+                $shipping_options['ship_separately'] = 'Ship CDs and DVDs <strong>separately</strong>';
+                $shipping_options['ship_together'] = 'Ship CDs and DVDs <strong>together</strong>';
+                
+                // If this is before last day to order and pick up at workshop,
+                // add that item to the array.
+                if ( strtotime(date('Y-m-d')) < strtotime(Config::get('workshop.last_pickup_order_date')) ) {
+                    $shipping_options['ship_dvd_only'] = 'Pick up CDs at Workshop/Ship DVDs';
+                }
+            } elseif ( $count['CD'] > 0 ) {
+                $shipping_options['ship_cd'] = 'Ship CDs';
+                if ( strtotime(date('Y-m-d')) < strtotime(Config::get('workshop.last_pickup_order_date')) ) {        
+                    $shipping_options['pickup'] = 'Pick up CDs at Workshop';
+                }
+            } elseif ( $count['DVD'] > 0 ) {
+                $shipping_options['ship_dvd'] = 'Ship DVDs';
+            } elseif ( $count['MP3'] > 0 ) {
+                $shipping_options['mp3_only'] = 'MP3s only';
+            }
+            
+            return $shipping_options;          
+        }
+        
+        private function getCountOfItems() {
+            $count = array(
+                'CD' => 0,
+                'DVD' => 0,
+                'MP3' => 0,
+            );
+            
+            // Use shopping cart, if still populated...
+            if ( Cart::contents() ) {
+                $cartContents = Cart::contents();
+
+                foreach ( $cartContents as $cartItem ) {
+                    if ( $cartItem->prod_type == 'SET' ) {
+                        if ( substr($cartItem->form_id, 0, 1) == 'C' ) {
+                            $count['CD'] += $cartItem->unit_count * $cartItem->quantity;
+                        } else {
+                            $count[substr($cartItem->form_id, 0, 3)] += $cartItem->unit_count * $cartItem->quantity;
+                        }
+                    } else {
+                        $count[$cartItem->prod_type] += $cartItem->unit_count * $cartItem->quantity;
+                    }
+                }
+            // ... Otherwise, use order items.
+            } else {
+                if ( $order_id > 0 ) {
+                    $orderItems = Order::find($order_id)->orderItems();
+                    
+                    foreach ( $orderItems as $orderItem ) {
+                        $product = $orderItem->product();
+                        if ( $product->prod_type == 'SET' ) {
+                            if ( substr($product->form_id, 0, 1) == 'C' ) {
+                                $count['CD'] += $product->unit_count * $cartItem->quantity;
+                            } else {
+                                $count[substr($cartItem->form_id, 0, 3)] += $cartItem->unit_count * $cartItem->quantity;
+                            }
+                        } else {
+                            $count[$cartItem->prod_type] += $cartItem->unit_count * $cartItem->quantity;
+                        }
+                    }
+                }
+            }
+            
+            return $count;
         }
 }
--- app/views/orders/create.blade.php
+++ app/views/orders/create.blade.php
@@ -1,7 +1,33 @@
 @section('main')
-	<h2>Create Project</h2>
+	<h2>Create Order</h2>
+        
+        @include('products/partials/_cart')
 
-  {{ Form::model(new Project, ['route' => ['projects.store']]) }}
-    @include('projects/partials/_form', ['submit_text' => 'Create Project'])
-  {{ Form::close() }}
+    {{ Auth::user()->first_name }}&nbsp;{{ Auth::user()->last_name }}
+    
+    
+    {{ Form::model(new Order, array('route' => array('orders.store'), 'method' => 'post', 'role' => 'form', 'class' => 'form-horizontal')) }}
+            <div class='col-xs-9'>
+                <div class="form-group floating-label-form-group">
+                    {{ Form::label('shipping_option', 'Delivery Method', array('class' => 'control-label control-label-reqd col-xs-5')) }}
+                    <div class="input-group col-xs-6">
+                        <!--<span class="hovertext">Please enter your first (given) name.<div class="triangle"></div></span> -->
+                        <span class="input-group-addon"><i class="fa fa-plane fa-fw"></i></span> 
+                        {{ Form::select('shipping_option', $shipping_options, NULL, array('class' => 'form-control input-sm input-sm-reqd floatlabel')) }}
+                    </div>
+                </div>                
+                <div class="form-group floating-label-form-group">
+                    {{ Form::label('order_notes', 'Order Notes', array('class' => 'control-label control-label-reqd col-xs-5')) }}
+                    <div class="input-group col-xs-6">
+                        <!--<span class="hovertext">Please enter your last name (surname).<div class="triangle"></div></span> -->
+                        <span class="input-group-addon"><i class="fa fa-file-text-o fa-fw"></i></span>
+                        {{ Form::textarea('order_notes', 
+                                    'Order Notes',
+                                    null, array('class' => 'form-control input-sm input-sm-reqd floatlabel', 
+                                            'placeholder' => 'Order Notes', 
+                                            'data-label' => 'Please enter any notes or special instructions about your order. (Optional)')) }}
+                    </div>
+                </div>
+            </div>
+    {{ Form::close() }}
 @stop
