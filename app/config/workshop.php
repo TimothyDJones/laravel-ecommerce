8689793db0dcdcf38681208f89fadb28a1604796
Tim Jones <tdjones74021@yahoo.com>
2015-02-04 14:26:02 -0600

    Add 'shipping_options' array to configuration options.

--- app/config/workshop.php
+++ app/config/workshop.php
@@ -4,5 +4,14 @@ return array(
     'current_workshop_year' => 2014,
     'last_pickup_order_date' => '2015-03-15',
     'last_preorder_discount_date' => '2015-03-10',
+    'shipping_options' => array(
+        'ship_together', 
+        'ship_separately', 
+        'ship_dvd_only', 
+        'ship_cd',
+        'pickup',
+        'ship_dvd',
+        'mp3_only',
+    ),
 );
 
