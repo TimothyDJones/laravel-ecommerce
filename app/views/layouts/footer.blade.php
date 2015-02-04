db8a77ba5ed72d47aa3bf248dac2ae5512a48cca
Tim Jones <tdjones74021@yahoo.com>
2015-02-03 20:46:28 -0600

    Update links in footer bar.

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -4,13 +4,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-4">
-                    
+                    {{ link_to_route('items.index', 'Free Downloads', NULL, array('class' => 'text-center')) }}
+                <div class="col-xs-4">
+                    {{ link_to_route('products.index', 'Order CDs, DVDs, and MP3s', NULL, array('class' => 'text-center')) }}
                 </div>
                 <div class="col-xs-4">
-                    Place Order
-                </div>
-                <div class="col-xs-4">
-                    &copy;&nbsp;2008&nbsp;-&nbsp;<?php echo date("Y"); ?> <strong>{{ HTML::link('http://www.workshopmultimedia.com', 'Workshop Multimedia', ['id' => 'wmmUrl']); }}</strong>
+                    &copy;&nbsp;2008&nbsp;-&nbsp;<?php echo date("Y"); ?> <strong>{{ HTML::link('http://www.workshopmultimedia.com', 'Workshop Multimedia', array('id' => 'wmmUrl', 'class' => 'text-center')); }}</strong>
                 </div>
             </div>
         </div>
