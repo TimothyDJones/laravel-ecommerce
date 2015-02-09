baf30f9b21b050fca7413baaab5a6c919ea9f064
Tim Jones <tdjones74021@yahoo.com>
2015-02-08 18:31:42 -0600

    Change 'current' workshop year to 2015.

--- app/config/workshop.php
+++ app/config/workshop.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'current_workshop_year' => 2014,
+    'current_workshop_year' => 2015,
     'last_pickup_order_date' => '2015-03-15',
     'last_preorder_discount_date' => '2015-03-10',
     'shipping_options' => array(
