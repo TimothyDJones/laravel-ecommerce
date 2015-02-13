1f1e705f88f1436b6df8051fa6a8fa86fe2fb7e2
Tim Jones <tdjones74021@yahoo.com>
2015-02-13 14:18:22 -0600

    Change array syntax to traditional syntax to support PHP 5.3.

--- app/views/customers/login.blade.php
+++ app/views/customers/login.blade.php
@@ -1,10 +1,10 @@
 @section('main')
     {{ Form::open(array('action' => 'CustomersController@login')) }}
         {{ Form::label('email', 'E-mail Address:') }}
-        {{ Form::text('email', null, ['placeholder' => 'E-mail Address']) }}
+        {{ Form::text('email', null, array('placeholder' => 'E-mail Address')) }}
         {{ Form::label('password', 'Password:') }}
-        {{ Form::password('password', null, ['placeholder' => 'Password']) }}
-        {{ Form::submit('Log In', ['class' => 'btn btn-primary btn-sm']) }}
+        {{ Form::password('password', null, array('placeholder' => 'Password')) }}
+        {{ Form::submit('Log In', array('class' => 'btn btn-primary btn-sm')) }}
     {{ Form::close() }}
     
     @if ( Auth::guest() )
