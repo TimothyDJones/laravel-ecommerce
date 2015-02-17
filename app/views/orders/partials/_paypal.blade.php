e68d2d669a9c4c2bb1974152a2486282df787f25
Tim Jones <tdjones74021@yahoo.com>
2015-02-17 15:38:29 -0600

    Add CR/LF after each Paypal hidden attribute. Add 'id' value for each Paypal hidden attribute.

--- app/views/orders/partials/_paypal.blade.php
+++ app/views/orders/partials/_paypal.blade.php
@@ -1,6 +1,6 @@
         {{ Form::open( array('class' => 'inline', 'url' => $paypal_attrs['form_action_url'] . 'cgi-bin/webscr', 'method' => 'POST') ) }}
             @foreach ( array_except($paypal_attrs, 'form_action_url') as $key => $value )
-            {{ Form::hidden($key, $value) }}
+            {{ Form::hidden($key, $value, array('id' => $key)) }}{{ "\n" }}
             @endforeach
             {{ Form::submit('Pay Now', array('class' => 'btn btn-lg btn-success',
                                                     'data-toggle' => 'tooltip',
