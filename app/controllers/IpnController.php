096a28631701385876a764aee9c37678f8cdf8b6
Tim Jones <tdjones74021@yahoo.com>
2015-02-21 20:14:33 -0600

    Add functionality to update order with IPN data after successful IPN processing.

--- app/controllers/IpnController.php
+++ app/controllers/IpnController.php
@@ -9,12 +9,26 @@ class IpnController extends \BaseController {
 	 */
 	public function store()
 	{
-            $order = IPN::getOrder();
+            Log::debug('IPN receiver - $_POST array: ' . print_r($_POST, TRUE));
+            $ipnOrder = IPN::getOrder();
+            
+            Log::debug('IPN receiver - After processing... IPN order: ' . print_r($ipnOrder, TRUE));
+            
+            $orderItem = $ipnOrder->items->first();
+            
+            $order = Order::find((int) $orderItem->item_number);
+            $order->paypal_txn_id = $ipnOrder->txn_id;
+            $order->paypal_fee = $ipnOrder->mc_fee;
+            $order->ipn_order_id = $ipnOrder->id;
+            //$order->id = (int) $orderItem->item_number;
+            if ( $ipnOrder->memo ) $order->order_notes .= "\n\n" . $ipnOrder->memo;
+            $order->order_status = 'Payment Received';
+            $order->save();
             
             // After the order is persisted to IPN tables,
             // we redirect to the main order processing passing
             // the txn_id as the key.
-            return Redirect::route('orders.complete', array());
+            return Redirect::route('orders-complete', $order->id);
 	}
 
 
