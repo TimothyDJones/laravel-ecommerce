58786cf407b18cd3f3be6c25d32d37531b860254
Tim Jones <tdjones74021@yahoo.com>
2015-02-23 16:43:45 -0600

    Add button for administrator users to re-send confirmation e-mail.

--- app/views/orders/partials/_order_completion.blade.php
+++ app/views/orders/partials/_order_completion.blade.php
@@ -1,4 +1,5 @@
         <h2>Order Completion Confirmation</h2>
+        <h3>Workshop Multimedia CD/DVD/MP3 Order #{{ $order->id }}</h3>
         <div class="row">
             <div class="col-md-8 alert alert-info">
                 <p>
@@ -7,7 +8,7 @@
                 </p>
                 <p>
                     If you have any questions about your order, do not hesitate to contact 
-                    us by telephone at <strong>(918) 260 9081</strong> or e-mail at 
+                    us by telephone at <strong>(918) 260 9084</strong> or e-mail at 
                     <strong><a href='mailto:orders@workshopmultimedia.com'>orders@workshopmultimedia.com</a></strong>.
                 </p>
             </div>
@@ -19,4 +20,13 @@
             <p>Regards,</p>
             <p><a href="http://www.workshopmultimedia.com">Workshop Multimedia</a></p>
         </div>
+        
+        @if ( Session::get('AdminUser') )
+        <div class="row pull-right">
+            {{ link_to_route('resend-order-email', 'Resend E-mail', $order->id, array('class' => 'btn btn-sm btn-danger outline',
+                                                                    'data-toggle' => 'tooltip',
+                                                                    'data-placement' => 'left',
+                                                                    'data-original-title' => 'Resend order confirmation e-mail to customer.')) }}
+        </div>                                                    
+        @endif
 
