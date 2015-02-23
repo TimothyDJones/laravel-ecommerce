3c1988a5de646111104931c9cf480fbcf14e883e
Tim Jones <tdjones74021@yahoo.com>
2015-02-23 16:48:52 -0600

    Add "clear" method to display to fix any inadvertant layout problems.

--- app/views/orders/partials/_order_details.blade.php
+++ app/views/orders/partials/_order_details.blade.php
@@ -16,6 +16,7 @@
                 <p><strong>Order Notes</strong>: {{ '[None]' or $order->order_notes }}</p>
             </div>            
         </div>
+        <div class="clear"></div>
         <div class="row">
             <h3>Order Summary</h3>
             {{-- @include('products/partials/_cart') --}}
