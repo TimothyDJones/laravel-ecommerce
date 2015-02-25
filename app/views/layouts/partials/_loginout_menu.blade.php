950d596bd4ecd59e3f86324f09c3c0e51a600cc8
Tim Jones <tdjones74021@yahoo.com>
2015-02-25 17:23:52 -0600

    Add 'Admin' navbar item, which is shown *ONLY* if user is admin.

--- app/views/layouts/partials/_loginout_menu.blade.php
+++ app/views/layouts/partials/_loginout_menu.blade.php
@@ -2,6 +2,9 @@
                             <li>{{ link_to('login', 'Log In') }}</li>
                         @endif
                         @if ( Auth::check() )
+                            @if ( Auth::user()->admin_ind )
+                            <li>{{ link_to('admin', 'Admin') }}</li>
+                            @endif
                             <li>{{ link_to('profile', 'Profile', array('title' => Auth::user()->email)) }}</li>
                             <li>{{ link_to('logout', 'Log Out', array('title' => Auth::user()->email)) }}</li>
                         @endif
