3bd229eca233d8a7f85004e2f82c3cb043b520f7
Tim Jones <tdjones74021@yahoo.com>
2015-02-25 16:30:59 -0600

    Add user e-mail address to Profile and Log Out links using 'title' (hover) attribute.

--- app/views/layouts/partials/_loginout_menu.blade.php
+++ app/views/layouts/partials/_loginout_menu.blade.php
@@ -2,7 +2,7 @@
                             <li>{{ link_to('login', 'Log In') }}</li>
                         @endif
                         @if ( Auth::check() )
-                            <li>{{ link_to('profile', 'Profile') }}</li>
-                            <li>{{ link_to('logout', 'Log Out') }}</li>
+                            <li>{{ link_to('profile', 'Profile', array('title' => Auth::user()->email)) }}</li>
+                            <li>{{ link_to('logout', 'Log Out', array('title' => Auth::user()->email)) }}</li>
                         @endif
 
