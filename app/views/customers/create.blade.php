9158f4bb4a1f65d45b763173060ac85a2599fac1
Tim Jones <tdjones74021@yahoo.com>
2015-02-27 14:39:43 -0600

    Change to traditional PHP array syntax for from attributes.

--- app/views/customers/create.blade.php
+++ app/views/customers/create.blade.php
@@ -1,7 +1,7 @@
 @section('main')
 	<h2>Register Customer</h2>
 
-        {{ Form::model(new Customer, ['route' => ['customers.store'], 'role' => 'form', 'class' => 'form-horizontal']) }}
+        {{ Form::model(new Customer, array('route' => ['customers.store'], 'role' => 'form', 'class' => 'form-horizontal')) }}
             @include('customers/partials/_form', array('submit_button_label' => 'Register'))
 	{{ Form::close() }}
 
