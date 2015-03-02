358ad0e5c5feac01d4c5a0828645c299b5649317
Tim Jones <tdjones74021@yahoo.com>
2015-03-02 15:44:58 -0600

    Add 'search' functionality and re-factored code to use common functionality to display search results and 'index' page data.

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -10,11 +10,7 @@ class ProductsController extends \BaseController {
 	public function index($workshop_year = NULL)
 	{
             $current_workshop_year = (int) Config::get('workshop.current_workshop_year');
-            /*
-            if ( !$workshop_year ) {
-                $products = Product::paginate(20);
-                //$products = Product::all();
-            } else { */
+
             if ( $workshop_year < 2008  || $workshop_year > $current_workshop_year ) {
                 $workshop_year = $current_workshop_year;
             }
@@ -33,26 +29,9 @@ class ProductsController extends \BaseController {
             $results = $query->remember(5)->get();
 
             Log::info('ProductsController@index - Count of query results: ' . $results->count(), array($results));
-
-            // Paginate the results of the custom query by using 'Paginator::make()'.
-            // http://stackoverflow.com/a/23881516
-            $paginator = json_decode($results);
-            $perPage = 20;
-            $page = Input::get('page', 1);
-            if ( $page > count($paginator) or $page < 1 ) { $page = 1; }
-            $offset = ($page * $perPage) - $perPage;
-            $dataSubset = array_slice($paginator, $offset, $perPage);
-            $products = Paginator::make($dataSubset, count($paginator), $perPage);   
             
-            $this->layout->content = View::make('products.index', compact('products'))
-                    ->with(array(
-                        'heading' => 'Product List', 
-                        'search_criteria' => NULL,
-                        'workshop_year_selected' => $workshop_year,
-                        'workshop_year_list' => ProductsController::getWorkshopYearList(),
-                        'orderVerification' => FALSE,
-                        'mp3_tooltip' => "Enable/check to order MP3 for $" . Config::get('workshop.unit_price_list')['MP3'] . " instead of CD.",
-                    ));
+            ProductsController::makeIndexView($results, array('workshop_year_selected' => $workshop_year));
+
 	}
 
 
@@ -132,6 +111,82 @@ class ProductsController extends \BaseController {
         public function changeWorkshopYear() {
             $workshop_year = Input::get('workshop_year_select');
             return Redirect::to('products/' . $workshop_year);
+        }
+        
+        public function search() {
+            $search_year_ind = Utility::nvl(Input::get('search_year_ind'), 'All');
+            $input = urldecode(Input::get('search'));
+            if ( !empty( $input ) ) {
+                $searchTerms = explode(',', $input);
+                
+                $query = Product::orderBy('workshop_year', 'desc')
+                        ->orderBy('id', 'asc');
+                // Assign index 0 search terms directly...
+                $query->where('speaker_last_name', 'LIKE', '%' . $searchTerms[0] . '%')
+                        ->orWhere('speaker_first_name', 'LIKE', '%' . $searchTerms[0] . '%')
+                        ->orWhere('session_title', 'LIKE', '%' . $searchTerms[0] . '%');
+                foreach ( $searchTerms as $key => $value ) {
+                    if ($key > 0)   // Skip index 0; see above
+                    {
+                        $query->orWhere('speaker_last_name', 'LIKE', '%' . $searchTerms[$key] . '%')
+                        ->orWhere('speaker_first_name', 'LIKE', '%' . $searchTerms[$key] . '%')
+                        ->orWhere('session_title', 'LIKE', '%' . $searchTerms[$key] . '%');
+                    }
+                }
+                
+                $current_workshop_year = Config::get('workshop.current_workshop_year');
+                if ( $search_year_ind == 'Current' ) {
+                    $query->where('workshop_year', '=', $current_workshop_year);
+                }
+                
+                $results = $query->remember(5)->get();  //->paginate(20);
+                
+                $message = "Search results for search criteria '" . $input . "'.";
+                
+                ProductsController::makeIndexView($results, 
+                        array('search_criteria' => $input, 
+                            'search_year_ind' => $search_year_ind,
+                            'message' => $message,
+                            ));
+
+            }            
+            
+        }
+
+	/**
+	 * Build display of products from search results, default, etc.
+         * 
+         * @param $results - array of 'Product' items from search results
+	 * @param $params - array of additional view variables to send (e.g.,
+         *          search criteria, workshop year selection, etc.)
+         * 
+	 * @return Response
+	 */        
+        private function makeIndexView($results, $params) {
+            // Paginate the results of the custom query by using 'Paginator::make()'.
+            // http://stackoverflow.com/a/23881516
+            $paginator = json_decode($results);
+            $perPage = 20;
+            $page = Input::get('page', 1);
+            if ( $page > count($paginator) or $page < 1 ) { $page = 1; }
+            $offset = ($page * $perPage) - $perPage;
+            $dataSubset = array_slice($paginator, $offset, $perPage);
+            $products = Paginator::make($dataSubset, count($paginator), $perPage);
+            
+            $params['heading'] = 'Product List';
+            $params['workshop_year_list'] = ProductsController::getWorkshopYearList();
+            $params['orderVerification'] = FALSE;
+            $params['mp3_tooltip'] = "Enable/check to order MP3 for $" 
+                    . Config::get('workshop.unit_price_list')['MP3'] 
+                    . " instead of CD.";
+            if (!isset($params['workshop_year_selected']))
+                $params['workshop_year_selected'] = Config::get('workshop.current_workshop_year');
+            if (!isset($params['search_year_ind']))
+                $params['search_year_ind'] = 'All';
+            if (!isset($params['search_criteria']))
+                $params['search_criteria'] = NULL;
+            
+            $this->layout->content = View::make('products.index', compact('products'))->with($params);            
         }
         
         public function showCart() {
