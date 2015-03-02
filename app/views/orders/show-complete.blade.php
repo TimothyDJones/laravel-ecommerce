1e4170cf949f8f10f8a91a67549727d84db2aedb
Tim Jones <tdjones74021@yahoo.com>
2015-03-01 19:47:09 -0600

    Display 'hashkey' value from session data when in "debug" mode (app/config.php).

--- app/views/orders/show-complete.blade.php
+++ app/views/orders/show-complete.blade.php
@@ -1,4 +1,7 @@
 @section('main')
+    @if ( Config::get('app.debug') && Session::has('hashkey') )
+        {{ Kint::dump( Session::get('hashkey') ) }}
+    @endif
     @include('orders/partials/_order_completion')
 @stop
 
