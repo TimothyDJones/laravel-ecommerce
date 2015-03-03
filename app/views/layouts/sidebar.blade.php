2f955b36f4344ab44ac077eb3ee9cc2f1037f991
Tim Jones <tdjones74021@yahoo.com>
2015-03-03 14:20:41 -0600

    Add weather widget to sidebar. Remove link to free downloads from sidebar.

--- app/views/layouts/sidebar.blade.php
+++ app/views/layouts/sidebar.blade.php
@@ -3,7 +3,15 @@
         <ul class="nav nav-pills nav-stacked">
             <li><a href="http://www.tulsaworkshop.org/">Tulsa Workshop</a></li>
             <li><a href="http://www.memorialdrive.org/">Memorial Drive Church of Christ</a></li>
-            <li> {{ link_to('items', 'Free Downloads') }}</li>
+            {{-- <li> {{ link_to('items', 'Free Downloads') }}</li> --}}
         </ul>
+        
+        <div style="width: 180px;"><iframe style="display: block;" 
+                                           src="http://cdnres.willyweather.com/widget/loadView.html?id=15903" 
+                                           width="180" height="228" frameborder="0" scrolling="no"></iframe>
+            <a style="margin: -20px 0 0 0;z-index: 1;position: relative;display: block;
+               text-indent: -9999em;height: 20px" 
+               href="http://www.willyweather.com/ok/tulsa-county/tulsa-international-airport.html">willyweather</a>
+        </div>
     </nav>
 
