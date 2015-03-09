1f8789e9e94aaaa525b443285937e3c55263e969
Tim Jones <tdjones74021@yahoo.com>
2015-03-08 20:21:53 -0500

    Fix layout/formating of "Order Notes" text area.  (Due to problems with Laravel Blade "Form" element had to use standard form element syntax.)

--- app/views/orders/partials/_order_options_form.blade.php
+++ app/views/orders/partials/_order_options_form.blade.php
@@ -1,7 +1,6 @@
                 <div class="form-group floating-label-form-group">
                     {{ Form::label('shipping_option', 'Delivery Method', array('class' => 'control-label control-label-reqd col-xs-3')) }}
                     <div class="input-group col-xs-6">
-                        <!--<span class="hovertext">Please enter your first (given) name.<div class="triangle"></div></span> -->
                         <span class="input-group-addon"><i class="fa fa-plane fa-fw"></i></span> 
                         {{ Form::select('shipping_option', $shipping_options, NULL, array('class' => 'form-control input-sm input-sm-reqd floatlabel',
                                                                                             'data-toggle' => 'tooltip',
@@ -12,12 +11,9 @@
                 <div class="form-group floating-label-form-group">
                     {{ Form::label('order_notes', 'Order Notes', array('class' => 'control-label control-label-reqd col-xs-3')) }}
                     <div class="input-group col-xs-6">
-                        <!--<span class="hovertext">Please enter your last name (surname).<div class="triangle"></div></span> -->
                         <span class="input-group-addon"><i class="fa fa-file-text-o fa-fw"></i></span>
-                        {{ Form::textarea('order_notes', 
-                                    'Order Notes',
-                                    null, array('class' => 'form-control input-sm input-sm-reqd floatlabel', 
-                                            'placeholder' => 'Order Notes', 
-                                            'data-label' => 'Please enter any notes or special instructions about your order. (Optional)')) }}
+                        <textarea id="order_notes" name="order_notes"
+                                  style="width: 100%;" rows="4"
+                                  class="form-control input-sm input-sm-reqd floatlabel">Order Notes</textarea>
                     </div>
                 </div>
