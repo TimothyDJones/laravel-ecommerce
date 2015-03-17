5e10daeb4f7194d32575217edc7d0ee493518341
Tim Jones <tdjones74021@yahoo.com>
2015-03-17 12:38:36 -0500

    Move 'Add Customer' to display on right side of page.

--- app/views/admin/partials/_add_customer_order.blade.php
+++ app/views/admin/partials/_add_customer_order.blade.php
@@ -1,3 +1,3 @@
     <div class="row">
-        {{ link_to_route('customers.create', 'Add Customer', NULL, array('class' => 'btn btn-lg btn-primary')) }}
+        {{ link_to_route('customers.create', 'Add Customer', NULL, array('class' => 'btn outline btn-primary pull-right')) }}
     </div>
