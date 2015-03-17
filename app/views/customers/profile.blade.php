ab47e6773e8ea024088cb6b79525dd710e9a127a
Tim Jones <tdjones74021@yahoo.com>
2015-03-17 12:14:52 -0500

    Update customer 'profile' page to display admin "Create Order" button under "Orders" heading (instead of normal user 'New Order' button).

--- app/views/customers/profile.blade.php
+++ app/views/customers/profile.blade.php
@@ -19,7 +19,7 @@
             <p>{{ $customer->telephone2 }}</p>
             @endif
             
-            @if ( (Auth::check() && Auth::user()->admin_ind) || Auth::id() == $customer->id )
+            @if ( Auth::check() && ( Auth::user()->admin_ind > 0 || Auth::id() == $customer->id ) )
             {{ link_to_route('customers.edit', 'Edit', $customer->id, array('class' => 'btn btn-info')) }}
             @endif
         </div>
@@ -32,7 +32,7 @@
             @endif
             <p>{{ $customer->address->city }}, {{ $customer->address->state }} {{ $customer->address->postal_code }} {{ $customer->address->country }}</p>
             
-            @if ( (Auth::check() && Auth::user()->admin_ind) || Auth::id() == $customer->id )
+            @if ( Auth::check() && ( Auth::user()->admin_ind > 0 || Auth::id() == $customer->id ) )
             {{ link_to_route('customers.addresses.edit',
                         'Edit', array($customer->id, $customer->address->id), array('class' => 'btn btn-info')) }}
             @endif            
@@ -41,14 +41,18 @@
         <div class="col-md-4">
             <h3>Orders</h3>
             @foreach ( $orders as $order )
-                @if ( Auth::id() == $customer->id || (int) $customer->admin_ind > 0 )
+                @if ( Auth::check() && (Auth::id() == $customer->id || (int) $customer->admin_ind > 0 ) )
                 <p>{{ link_to_route('orders.show', $order->id, $order->id) }} ({{ $order->order_status }})</p>
                 @else
                 <p>{{ $order->id }} ({{ $order->order_status }})</p>
                 @endif
             @endforeach
             
-            {{ link_to_route('products.index', 'New Order', NULL, array('class' => 'btn btn-info')) }}
+            @if ( Auth::check() && Auth::user()->admin_ind )
+                {{ link_to_route('admin-order-create', 'Create Order', $customer->id, array('class' => 'btn btn-primary')) }}
+            @elseif ( Auth::check() && Auth::id() == $customer->id )
+                {{ link_to_route('products.index', 'New Order', NULL, array('class' => 'btn btn-info')) }}
+            @endif
         </div>
     </div>
 @stop
